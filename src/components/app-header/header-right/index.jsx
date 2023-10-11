3aaea3c8b1451f87104cdd67b2afe9251de22968
Milk-Ice <mathila1218@gmail.com>
2023-10-11 12:25:19 +0800

    02头部布局-Profile面板切换（监听window，解决事件冒泡问题）

--- src/components/app-header/header-right/index.jsx
+++ src/components/app-header/header-right/index.jsx
@@ -1,10 +1,26 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import { HeaderRightWrapper } from './style'
 import IconAvatar from '@/assets/css/svg/icon_avatar'
 import IconGobal from '@/assets/css/svg/icon_gobal'
 import IconMore from '@/assets/css/svg/icon_more'
 
 const HeaderRight = memo(() => {
+  const [ showPanel, setShowPanel ] = useState(false)
+
+  useEffect(()=> {
+    function windowHandleClick() {
+      setShowPanel(false)
+    }
+    window.addEventListener("click",windowHandleClick, true)
+    return () => {
+      window.removeEventListener("click",windowHandleClick,true)
+    }
+  },[])
+
+  function profileClickHandle() {
+    setShowPanel(true)
+  }
+
   return (
     <HeaderRightWrapper>
       <div className='right'>
@@ -13,11 +29,27 @@ const HeaderRight = memo(() => {
           <span className='item register'>注册</span>
           <IconGobal />
         </div>
-        <div className='profile'>
+        <div className='profile' onClick={profileClickHandle}>
          <IconMore />
          <IconAvatar />
+       
+        { showPanel && (
+          <div className='panel'>
+          <div className='top'>
+            <div className='item'>注册</div>
+            <div className='item'>登陆</div>
+          </div>
+          <div className='bottom'>
+            <div className='item'>出租房源</div>
+            <div className='item'>开展体验</div>
+            <div className='item'>帮助</div>
+          </div>
+        </div> 
+        )
+      }
         </div>
       </div>
+      
     </HeaderRightWrapper>
   )
 })
--- src/components/app-header/header-right/style.js
+++ src/components/app-header/header-right/style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 export const HeaderRightWrapper = styled.div`
 .right{
     display: flex;
+    position: relative;
     align-items: center;
     color: ${props => props.theme.text.primaryColor};
     .item{
@@ -13,7 +14,7 @@ export const HeaderRightWrapper = styled.div`
     margin-right: 20px;
   }  
   .profile{
-  
+
     display: flex;
     justify-content: space-evenly;
     align-items: center;
@@ -27,5 +28,26 @@ export const HeaderRightWrapper = styled.div`
    ${props => props.theme.mixins.boxShadow}
     
   }
+  .panel{
+    position: absolute;
+    width: 240px;
+    top: 60px;
+    right: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 2px 4px rgba(0,0,0,.1);
+    .top, .bottom{
+      padding: 10px 0px;
+    }
+      .item{
+        height: 40px;
+        line-height: 40px;
+        padding: 0 16px;
+      }
+      display: flex;
+      flex-direction: column;
+    }
+    .top {
+      border-bottom: 1px solid #ddd;
+    }
 }
 `
