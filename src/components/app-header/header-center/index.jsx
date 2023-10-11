1a757f15cc0407e1a0844fdb3ec268c28fa514b8
Milk-Ice <mathila1218@gmail.com>
2023-10-11 11:06:46 +0800

    02头部布局-center

--- src/components/app-header/header-center/index.jsx
+++ src/components/app-header/header-center/index.jsx
@@ -1,9 +1,15 @@
 import React, { memo } from 'react'
 import { HeaderCenterWrapper } from './style'
+import IconSearch from '@/assets/css/svg/icon_search'
 
 const HeaderCenter = memo(() => {
   return (
-    <HeaderCenterWrapper>HeaderCente</HeaderCenterWrapper>
+    <HeaderCenterWrapper>
+      <div className='search-bar'>
+        <div className='text'>搜索房源</div>
+        <IconSearch />
+      </div>
+    </HeaderCenterWrapper>
   )
 })
 
--- src/components/app-header/header-center/style.js
+++ src/components/app-header/header-center/style.js
@@ -1,2 +1,21 @@
 import styled from "styled-components";
-export const HeaderCenterWrapper = styled.div``
+export const HeaderCenterWrapper = styled.div`
+.search-bar{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 300px;
+    height: 48px;
+    padding: 0 9px;
+    border: 1px solid #ddd;
+    border-radius: 24px;
+    cursor: pointer;
+
+    ${props => props.theme.mixins.boxShadow}
+    .text{
+        padding: 0 16px;
+        font-weight: bold;
+    }
+}
+    
+`
