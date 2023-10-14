b4852563574598ea9f8d8759fe3022e84b87b8ff
Milk-Ice <mathila1218@gmail.com>
2023-10-14 12:06:52 +0800

    03_entire模块_过滤器功能(选中和取消选中)

---
+++ src/views/entire/c-cpns/entire-pagination/index.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+import React, { memo } from 'react'
+import { PageWrapper } from './style'
+
+const EntirePage = memo((props) => {
+  return (
+    <PageWrapper>
+        
+    </PageWrapper>
+  )
+})
+
+EntirePage.propTypes = {}
+
+export default EntirePage
---
+++ src/views/entire/c-cpns/entire-rooms/index.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+import React, { memo } from 'react'
+import { RoomWrapper } from './style'
+
+const EntireRooms = memo((props) => {
+  return (
+    <RoomWrapper>
+        
+    </RoomWrapper>
+  )
+})
+
+EntireRooms.propTypes = {}
+
+export default EntireRooms
