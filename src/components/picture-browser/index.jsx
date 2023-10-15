5db8fc59369dae6c22edae1c4527f535cfc558b0
Milk-Ice <mathila1218@gmail.com>
2023-10-15 19:14:20 +0800

    04_详情图片浏览___图片的预览(列表)

--- src/components/picture-browser/index.jsx
+++ src/components/picture-browser/index.jsx
@@ -4,10 +4,13 @@ import { BrowserWrapper } from './style'
 import IconPictureClose from '@/assets/css/svg/icon_pictureClose'
 import IconArrowLeft from '@/assets/css/svg/icon_ArrowLeft'
 import IconArrowRight from '@/assets/css/svg/icon_ArrowRight'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import { StyleSheetManager } from 'styled-components'
 
 const PictureBrowser = memo((props) => {
   const { pictureUrls, closeClick } = props
   const [currentIndex, setCurrentIndex] = useState(0)
+  const [isNext, setisNext] = useState(true)
   useEffect(() => {
     document.body.style.overflow = "hidden"
     return () => {
@@ -25,33 +28,57 @@ const PictureBrowser = memo((props) => {
     if (newIndex > pictureUrls.length - 1) newIndex = 0
 
     setCurrentIndex(newIndex)
+    setisNext(isNext)
   }
+  // console.log(currentIndex, isNext)
   return (
-    <BrowserWrapper>
-      <div className="top">
-        <div className="close-btn" onClick={closeBtnClickHandle}>
-          <IconPictureClose fill="#fff" />
+    <BrowserWrapper isNext={isNext}>
+    <div className='top'>
+      <div className='close-btn' onClick={closeBtnClickHandle}>
+        <IconPictureClose/>
+      </div>
+    </div>
+    <div className='slider'>
+      <div className='control'>
+        <div className='btn left' onClick={e => controlClickHandle(false)}>
+          <IconArrowLeft width="77" height="77"/>
+        </div>
+        <div className='btn right' onClick={e => controlClickHandle(true)}>
+          <IconArrowRight width="77" height="77"/>
         </div>
       </div>
-      <div className="silder">
-        <div className="control">
-          <div className="btn left" onClick={e => controlClickHandle(false)}><IconArrowLeft width="88" height="88" /></div>
-          <div className="btn right" onClick={e => controlClickHandle(false)} ><IconArrowRight width="88" height="88" /></div>
+      <div className='picture'>
+        <SwitchTransition mode='in-out'>
+          <CSSTransition
+            key={pictureUrls[currentIndex]}
+            classNames="pic"
+            timeout={200}
+          >
+            <img src={pictureUrls[currentIndex]} alt="" />
+          </CSSTransition>
+        </SwitchTransition>
+      </div>
+    </div>
+    <div className='preview'>
+      <div className='info'>
+        <div className='desc'>
+          <div className='count'>
+            <span>{currentIndex+1}/{pictureUrls.length}:</span>
+            <span>room apartment图片{currentIndex+1}</span>
+          </div>
         </div>
-        <div className="picture">
-        <img src={pictureUrls[currentIndex]} alt='' />
+        <div className='list'>
+        </div>
       </div>
-      </div>
-      
-      <div className="preview">
-
-      </div>
-    </BrowserWrapper>
+    </div>
+  </BrowserWrapper>
   )
 })
 
 PictureBrowser.propTypes = {
-  pictureUrls: PropTypes.array
+  pictureUrls: PropTypes.array,
+  closeClick: PropTypes.func,
+  isNext: PropTypes.bool, // 添加 isNext 到 propTypes
 }
 
 export default PictureBrowser
