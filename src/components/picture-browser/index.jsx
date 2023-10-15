dd3c9472d686f9ef89b0d013c3e2b7697b52d0d1
Milk-Ice <mathila1218@gmail.com>
2023-10-15 17:25:53 +0800

    04_详情图片浏览___图片的切换

--- src/components/picture-browser/index.jsx
+++ src/components/picture-browser/index.jsx
@@ -1,25 +1,50 @@
 import PropTypes from 'prop-types'
-import React, { memo, useEffect } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import { BrowserWrapper } from './style'
 import IconPictureClose from '@/assets/css/svg/icon_pictureClose'
+import IconArrowLeft from '@/assets/css/svg/icon_ArrowLeft'
+import IconArrowRight from '@/assets/css/svg/icon_ArrowRight'
 
 const PictureBrowser = memo((props) => {
   const { pictureUrls, closeClick } = props
+  const [currentIndex, setCurrentIndex] = useState(0)
   useEffect(() => {
     document.body.style.overflow = "hidden"
     return () => {
       document.body.style.overflow = "auto"
     }
   }, [])
+  // 点击关闭
   function closeBtnClickHandle() {
     if (closeClick) closeClick()
+  }
+  // 点击下一个、上一个切换照片
+  function controlClickHandle(isNext = true) {
+    let newIndex = isNext ? currentIndex + 1 : currentIndex - 1
+    if (newIndex < 0) newIndex = pictureUrls.length - 1
+    if (newIndex > pictureUrls.length - 1) newIndex = 0
+
+    setCurrentIndex(newIndex)
   }
   return (
     <BrowserWrapper>
       <div className="top">
         <div className="close-btn" onClick={closeBtnClickHandle}>
-          <IconPictureClose fill="#fff"/>
+          <IconPictureClose fill="#fff" />
         </div>
+      </div>
+      <div className="silder">
+        <div className="control">
+          <div className="btn left" onClick={e => controlClickHandle(false)}><IconArrowLeft width="88" height="88" /></div>
+          <div className="btn right" onClick={e => controlClickHandle(false)} ><IconArrowRight width="88" height="88" /></div>
+        </div>
+        <div className="picture">
+        <img src={pictureUrls[currentIndex]} alt='' />
+      </div>
+      </div>
+      
+      <div className="preview">
+
       </div>
     </BrowserWrapper>
   )
--- src/components/picture-browser/style.js
+++ src/components/picture-browser/style.js
@@ -2,20 +2,73 @@ import styled from "styled-components";
 
 export const BrowserWrapper = styled.div`
     position: fixed;
+    z-index: 999;
     left: 0;
     right: 0;
     top: 0;
     bottom: 0;
+    display: flex;
+    flex-direction: column;
 
     background-color: #333;
     .top {
         position: relative;
         height: 86px;
+
         .close-btn {
             position: absolute;
             top: 15px;
             right: 26px;
         }
+    }
+    .silder {
+        position: relative;
+        display: flex;
+        justify-content: center;
+        flex: 1;
+        .control{
+            position: absolute;
+            z-index: 1;
+            left: 0;
+            right: 0;
+            top: 0;
+            display: flex;
+            justify-content: space-between;
+            bottom: 0;
+            color: #fff;
+            .btn {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 88px;
+                height: 100%;
+                cursor: pointer;
+            }
+        }
+            
+        .picture {
+        position: relative;
+        height: 100%;
+        overflow: hidden;
+        width: 100%;
+        max-width: 105vh;
+
+        img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            margin: 0 auto;
+            height: 100%;
+            user-select: none;
+        }
+        }
+        }
+    
+    .preview {
+        height: 100px;
+        margin-top: 10px;
+        background-color: purple;
     }
 
 `
